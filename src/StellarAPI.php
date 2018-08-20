5b63bd995e389e103ad1d2aa363de19adfc9a420
Fabian Kranen <fabian@unwind.nl>
2018-08-20 15:58:11 +0200

    Added more documentation

--- src/StellarAPI.php
+++ src/StellarAPI.php
@@ -3,7 +3,7 @@
 namespace GalacticBot;
 
 /*
-* Does the communication with the Stellar PHP API.
+* Wrapper class to simplify Stellar PHP API.
 */
 class StellarAPI {
 
@@ -15,27 +15,45 @@ class StellarAPI {
 
 	private $isTestNet;
 
+	/**
+	 * Private constructor, please use either the getTestNetAPI() or getPublicAPI() method.
+	 */
 	private function __construct($URL, $isTestNet) {
 		$this->URL = $URL;
 		$this->isTestNet = $isTestNet;
 	}
 
+	/**
+	 * Create a new instance of the StellarAPI class pointing to the test net API
+	 */
 	public static function getTestNetAPI() {
 		return new self("https://horizon-testnet.stellar.org/", true);
 	}
 
+	/**
+	 * Create a new instance of the StellarAPI class pointing to the public API
+	 */
 	public static function getPublicAPI() {
 		return new self("https://horizon.stellar.org/", false);
 	}
 
-	function getIsTestNet() { return $this->isTestNet; }
+	/**
+	 * Returns if the instance is pointing to the test net
+	 */
+	public function getIsTestNet() { return $this->isTestNet; }
 
+	/**
+	 * Retrieves the current order book from the public Stellar Horizon API for a specific asset pair
+	 */
 	static function getPublicOrderBook(Bot $bot, \ZuluCrypto\StellarSdk\XdrModel\Asset $sellingAsset, \ZuluCrypto\StellarSdk\XdrModel\Asset $buyingAsset, $limit = null) {
 		$api = new self("https://horizon.stellar.org/", false);
 
 		return $api->getOrderBook($bot, $sellingAsset, $buyingAsset, $limit);
 	}
 
+	/**
+	 * Retrieves the current order book from the Stellar Horizon API for a specific asset pair
+	 */
 	function getOrderBook(Bot $bot, \ZuluCrypto\StellarSdk\XdrModel\Asset $sellingAsset, \ZuluCrypto\StellarSdk\XdrModel\Asset $buyingAsset, $limit = null) {
 		$server = $this->isTestNet ? \ZuluCrypto\StellarSdk\Server::testNet() : \ZuluCrypto\StellarSdk\Server::publicNet();
 		$server = new ExtendedServer($server, $this->isTestNet);
@@ -47,6 +65,9 @@ class StellarAPI {
 		return $result;
 	}
 
+	/**
+	 * Retrieves all info known (trade list and whether it's still open or not) for an offer by its offerId
+	 */
 	function getOfferInfoByID(Bot $bot, $offerID) {
 		$server = $this->isTestNet ? \ZuluCrypto\StellarSdk\Server::testNet() : \ZuluCrypto\StellarSdk\Server::publicNet();
 		$server = new ExtendedServer($server, $this->isTestNet);
@@ -76,6 +97,9 @@ class StellarAPI {
 		return $response;
 	}
 
+	/**
+	 * Performs the Horizon API 'manageOffer' call (creates, updates or deletes an offer).
+	 */
 	function manageOffer(Bot $bot, Time $time, \ZuluCrypto\StellarSdk\XdrModel\Asset $sellingAsset, $sellingAmount, \ZuluCrypto\StellarSdk\XdrModel\Asset $buyingAsset, $offerIDToUpdate = null, $cancelOffer = false)
 	{
 		global $_BASETIMEZONE;
@@ -140,12 +164,18 @@ class StellarAPI {
 		return false;
 	}
 
+	/**
+	 * Get trades done in the past for a specific asset pair (from the pulic net)
+	 */
 	static function getPublicTradeAggregations(Bot $bot, \ZuluCrypto\StellarSdk\XdrModel\Asset $baseAsset, \ZuluCrypto\StellarSdk\XdrModel\Asset $counterAsset, Time $start, Time $end, $interval) {
 		$api = new self("https://horizon.stellar.org/", false);
 
 		return $api->getTradeAggregations($bot, $baseAsset, $counterAsset, $start, $end, $interval);
 	}
 
+	/**
+	 * Get trades done in the past for a specific asset pair
+	 */
 	function getTradeAggregations(Bot $bot, \ZuluCrypto\StellarSdk\XdrModel\Asset $baseAsset, \ZuluCrypto\StellarSdk\XdrModel\Asset $counterAsset, Time $start, Time $end, $interval) {
 		$limit = ($end->getTimestamp() - $start->getTimestamp()) / ($interval / 1000);
 		$limit = round($limit);
