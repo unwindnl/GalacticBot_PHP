c52fa44bd41e4894f1eb838e2c7142107d8470a6
Fabian Kranen <fabian@unwind.nl>
2019-01-08 10:55:32 +0100

    Moved Profiler class to its own file.

--- src/Bot.php
+++ src/Bot.php
@@ -824,25 +824,3 @@ abstract class Bot
 
 }
 
-
-class Profiler
-{
-
-	static $times = [];
-
-	static function start($name)
-	{
-		self::$times[$name] = microtime(true);
-	}
-
-	static function stop($name)
-	{
-		 $delta = microtime(true) - self::$times[$name];
-
-		 $delta = number_format($delta, 8);
-
-		 echo " -- Time $name = $delta\n";
-	}
-
-}
-
